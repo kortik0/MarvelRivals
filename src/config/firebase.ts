633dae30ea11ecf243c24ec72e0dcbbabc816a29
kortik0 <sveta220681@gmail.com>
2025-01-01 17:43:28 +0300

    oh no

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAd2b3owDq5i-ln-Hy0hxfp3sMDYovbk7A",
-  authDomain: "marvel-rivals-5f471.firebaseapp.com",
-  databaseURL: "https://marvel-rivals-5f471-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "marvel-rivals-5f471",
-  storageBucket: "marvel-rivals-5f471.firebasestorage.app",
-  messagingSenderId: "843645486210",
-  appId: "1:843645486210:web:9ebfd97429a7bfb4ea209b",
-  measurementId: "G-4QSVMSFMZV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
